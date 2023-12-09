51a38d05fd011c3dfcfb0bf1ec871dd09e5f2aa3
Michelle Chuen <michelle.chuen@hotmail.com>
2023-12-09 17:18:32 +0800

    test new data

--- include/js/main.js
+++ include/js/main.js
@@ -6,6 +6,7 @@ const scaling_for = {
     "author=moliere&play=l_ecole_des_femmes": 1.8,
     "author=moliere&play=le_medecin_malgre_lui": 1.7,
     "author=moliere&play=testing_test": 1.7,
+    "author=moliere&play=test_script": 1.7,
     "author=moliere&play=le_misanthrope": 1.8,
     "author=jean_racine&play=phedre": 2,
 };
