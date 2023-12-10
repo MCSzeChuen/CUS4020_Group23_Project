9026a55180295a92e0dce238d2753eaaff2788d6
Michelle Chuen <michelle.chuen@hotmail.com>
2023-12-10 17:43:54 +0800

    data testing

--- include/js/main.js
+++ include/js/main.js
@@ -2,9 +2,9 @@
 
 // Play scaling factors...
 const scaling_for = {
-    "author=disney&play=testing_test": 2,
-    "author=disney&play=test_script_v8": 1.8,
-    "author=moliere&play=l_avare": 1.7,
+    "author=moliere&play=l_avare": 2,
+    "author=disney&play=testing_test": 1.8,
+    "author=disney&play=test_script_v8": 2,
     // "author=moliere&play=l_ecole_des_femmes": 1.7,
     // "author=moliere&play=le_medecin_malgre_lui": 1.7,
     // "author=moliere&play=le_misanthrope": 1.8,
